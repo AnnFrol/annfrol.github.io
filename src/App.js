d228e83355fc780c679c01819d900b8fb2590a0f
AnnFrol <Ann.desi.d@gmail.com>
2024-08-28 00:37:06 +0500

    +

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
 import Projects from './Projects';
