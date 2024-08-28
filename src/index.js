6560f282387ae808e330766ebf2c98e7082db70a
AnnFrol <Ann.desi.d@gmail.com>
2024-08-29 00:14:50 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom'; 
 import './css/reset.css';
 import './css/index.css';
 import './css/App.css';
@@ -18,7 +18,7 @@ document.head.appendChild(link);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-	<Router basename="/">
+	<Router>
 	  <App />
 	</Router>
   </React.StrictMode>
