37b8ac12344d76d6947db773689603b0dc50d2cc
AnnFrol <Ann.desi.d@gmail.com>
2024-09-12 01:20:11 +0500

    Cube

--- src/PageLoader.js
+++ src/PageLoader.js
@@ -1,57 +1,70 @@
-import React, { useEffect, useRef } from 'react';
-import { gsap } from 'gsap';
+import React, { useEffect, useRef } from "react";
+import { gsap } from "gsap";
 
 // Определите customEase здесь, используя любую подходящую функцию easing GSAP
 const customEase = gsap.parseEase("power2.inOut");
 
 export function PageLoader({ onLoadingComplete }) {
-	const pageBgWrapRef = useRef(null);
-	const pageOverlayRef = useRef(null);
-	const redBgRef = useRef(null);
-	const greyBgRef = useRef(null);
+  const pageBgWrapRef = useRef(null);
+  const pageOverlayRef = useRef(null);
+  const colorBgRef = useRef(null);
+  const greyBgRef = useRef(null);
 
-	useEffect(() => {
-		const pageBgWrap = pageBgWrapRef.current;
-		const redBg = redBgRef.current;
-		const greyBg = greyBgRef.current;
+  useEffect(() => {
+    const pageBgWrap = pageBgWrapRef.current;
+    const colorBg = colorBgRef.current;
+    const greyBg = greyBgRef.current;
 
-		const pageToPageAnim = gsap.timeline({
-			onComplete: () => {
-				onLoadingComplete();
-			}
-		});
+    // Массив с цветами
+    const colors = ["#cde052", "#ff2a83", "#35c8bf"];
 
-		pageToPageAnim.to(redBg, {
-			y: "0%",
-			rotate: 0,
-			onStart: () => {
-				pageBgWrap.style.opacity = "1";
-			},
-			duration: 1.5,
-			ease: customEase,
-		});
+    // Случайный выбор цвета для pink-bg
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
 
-		pageToPageAnim.to(greyBg, {
-			y: "0%",
-			rotate: 0,
-			duration: 1.5,
-			ease: customEase,
-			onComplete: () => {
-				pageBgWrap.style.opacity = "0";
-			},
-		}, "-=1.35");
+    // Установить выбранный цвет для pink-bg
+    colorBg.style.backgroundColor = randomColor;
 
-		return () => {
-			pageToPageAnim.kill();
-		};
-	}, [onLoadingComplete]);
+    const pageToPageAnim = gsap.timeline({
+      onComplete: () => {
+        onLoadingComplete();
+      },
+    });
 
-	return (
-		<div id="loader" ref={pageBgWrapRef}>
-			<div id="loaderOverlay" ref={pageOverlayRef}></div>
-			<div className="pink-bg" ref={redBgRef}></div>
-			<div className="grey-bg" ref={greyBgRef}></div>
-		</div>
-	);
+    pageToPageAnim.to(colorBg, {
+      y: "0%",
+      rotate: 0,
+      onStart: () => {
+        pageBgWrap.style.opacity = "1";
+      },
+      duration: 1.5,
+      ease: customEase,
+    });
+
+    pageToPageAnim.to(
+      greyBg,
+      {
+        y: "0%",
+        rotate: 0,
+        duration: 1.5,
+        ease: customEase,
+        onComplete: () => {
+          pageBgWrap.style.opacity = "0";
+        },
+      },
+      "-=1.35"
+    );
+
+    return () => {
+      pageToPageAnim.kill();
+    };
+  }, [onLoadingComplete]);
+
+  return (
+    <div id="loader" ref={pageBgWrapRef}>
+      <div id="loaderOverlay" ref={pageOverlayRef}></div>
+      <div className="color-bg" ref={colorBgRef}></div>
+      <div className="grey-bg" ref={greyBgRef}></div>
+    </div>
+  );
 }
 export default PageLoader;
--- src/Projects.js
+++ src/Projects.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import './css/projects.css';
-import AnimatedText from './AnimatedText';
+import React from "react";
+import "./css/projects.css";
+import AnimatedText from "./AnimatedText";
 
 const Projects = () => {
   return (
-<main className="projects">
-	<div>
-	  <AnimatedText title="Projects" text="ProjectsText" />
-	</div>
-</main>
+    <main className="projects">
+      <div>
+        <AnimatedText title="Projects" />
+      </div>
+    </main>
   );
 };
-
 
 export default Projects;
