29913d0aa007539399da3fcc096594c317af23f2
AnnFrol <Ann.desi.d@gmail.com>
2024-11-07 01:39:21 +0500

    +

--- src/Process.js
+++ src/Process.js
@@ -192,7 +192,7 @@ const Process = () => {
           filter="blur(200px)"
           xmlns="http://www.w3.org/2000/svg"
         >
-          <circle r="48%" cx="50%" cy="50%" fill="var(--maxgreenyellow)" />
+          <circle r="48%" cx="50%" cy="50%" />
         </svg>
       </div>
 
